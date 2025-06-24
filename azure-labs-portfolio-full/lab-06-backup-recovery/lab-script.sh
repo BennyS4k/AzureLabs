3b52edecc80edb7bad9e9b6b04bfd7af1d137dfe
BennyS4k <141947236+BennyS4k@users.noreply.github.com>
2025-06-24 15:05:51 +0100

    Update lab-script.sh

--- azure-labs-portfolio-full/lab-06-backup-recovery/lab-script.sh
+++ azure-labs-portfolio-full/lab-06-backup-recovery/lab-script.sh
@@ -2,18 +2,18 @@
 
 # Create Recovery Services Vault
 az backup vault create \
-  --resource-group rg-labs-core \
+  --resource-group rg-bclabs-core \
   --name backup-vault \
   --location uksouth
 
 az backup vault backup-properties set \
   --name backup-vault \
-  --resource-group rg-labs-core \
+  --resource-group rg-bclabs-core \
   --backup-storage-redundancy LocallyRedundant
 
 # Enable backup for VM
 az backup protection enable-for-vm \
   --vault-name backup-vault \
-  --resource-group rg-labs-core \
+  --resource-group rg-bclabs-core \
   --vm vm-web01 \
   --policy-name DefaultPolicy
