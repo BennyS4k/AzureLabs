3361c1e8bab4d0c0bcf00155671a89db4feb512a
BennyS4k <141947236+BennyS4k@users.noreply.github.com>
2025-06-24 14:10:47 +0100

    Update lab-script.sh

--- azure-labs-portfolio-full/lab-02-vnet-subnets/lab-script.sh
+++ azure-labs-portfolio-full/lab-02-vnet-subnets/lab-script.sh
@@ -3,7 +3,7 @@
 # Create VNet and subnets
 az network vnet create \
   --name vnet-lab \
-  --resource-group rg-labs-core \
+  --resource-group rg-bclabs-core \
   --address-prefix 10.0.0.0/16 \
   --subnet-name subnet-vm \
   --subnet-prefix 10.0.1.0/24
@@ -11,5 +11,5 @@ az network vnet create \
 az network vnet subnet create \
   --name subnet-db \
   --vnet-name vnet-lab \
-  --resource-group rg-labs-core \
+  --resource-group rg-bclabs-core \
   --address-prefix 10.0.2.0/24
