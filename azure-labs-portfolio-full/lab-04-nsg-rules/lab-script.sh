93ec0b333e329a12e8187325b1342e36ff0502c3
BennyS4k <141947236+BennyS4k@users.noreply.github.com>
2025-06-24 14:48:21 +0100

    Update lab-script.sh

--- azure-labs-portfolio-full/lab-04-nsg-rules/lab-script.sh
+++ azure-labs-portfolio-full/lab-04-nsg-rules/lab-script.sh
@@ -2,21 +2,21 @@
 
 # Allow RDP to VM Web01
 az network nsg rule create \
-  --resource-group rg-labs-core \
+  --resource-group rg-bclabs-core \
   --nsg-name nsg-web01 \
   --name Allow-RDP \
   --priority 100 \
   --access Allow \
   --protocol Tcp \
   --direction Inbound \
-  --source-address-prefix <YOUR_PUBLIC_IP> \
+  --source-address-prefix <MY_PUBLIC_IP> \
   --source-port-range "*" \
   --destination-address-prefix "*" \
   --destination-port-range 3389
 
 # Block all inbound to Db01
 az network nsg rule create \
-  --resource-group rg-labs-core \
+  --resource-group rg-bclabs-core \
   --nsg-name nsg-db01 \
   --name Deny-All-Inbound \
   --priority 100 \
