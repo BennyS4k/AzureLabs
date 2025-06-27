1c2c5bd2e4b3e106a25540554e95ac4d272b3bc4
BennyS4k <141947236+BennyS4k@users.noreply.github.com>
2025-06-27 10:51:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = "c3fcdf9b-71f4-49ca-8a17-e07277591f19"
+  client_id       = "1b1d5bfc-6e7b-4b9a-827f-2b3d9fe2082c"
+  client_secret   = "aEg8Q~xQW9R6oitG43uo8PkZE~yQ_WMM98bRmaoR"
+  tenant_id       = "4f7487d1-4c54-4260-89e9-4ba6bfc22735"
 }
 
 resource "azurerm_resource_group" "example" {
